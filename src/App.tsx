74d24b392c99dd35870937b87e53409afe35e5d8
Mona Lisa <trongh1337@gmail.com>
2025-06-25 16:11:39 +0700

    Switched to vite. Added alot of stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import './App.css';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
-// Import components
-import MainPage from './pages/mainPage';
-import SplashCursor from './Animations/SplashCursor/SplashCursor';
+const queryClient = new QueryClient();
 
-const App:React.FC = () => {
-  return (
-    <div className="App">
-      <div className='elementShadow elementShadow1'></div>
-      <div className='elementShadow elementShadow2'></div>
-      <MainPage />
-      <SplashCursor />
-    </div>
-  );
-}
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
